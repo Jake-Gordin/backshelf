92eef3024d722364d85aa248c4e43197f897759f
George Gordin <gcgordin@gmail.com>
2026-06-16 10:08:40 -0600

    testing local mysql

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-//start listening for front end via express
+//definitions and requirements
 const express = require ('express');
+const mysql = require ('mysql');
+//begin listening on 5555 for front-end
 const app = express();
 const cors = require('cors');
 app.use(cors());
-app.get('/', (req, res) => {
-  res.send('Received on the live server!')
-  console.log('received message')
-});
 app.listen(5555, () => console.log(`Listening on port 5555`));
+//connect to local mysql
+const mySQLCon = mysql.createConnection({
+  host: "localhost",
+  user: "clerk",
+  password: "dbpass"
+})
+mySQLCon.connect(function(error) {
+  if (error) throw erorr;
+  console.log("connected to local mySQL");
+})
+//responses
+app.get('/', (req, res) => {
+  res.send('Received on the live server!');
+  console.log('received message');
+  var queryText = "select * from users;"
+  mySQLCon.query(queryText, function (error, result) {
+    if (error) throw error;
+    res.send('SQL Results: ' + result)
+  })
+});
