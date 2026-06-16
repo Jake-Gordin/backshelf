1b7c48684b00dc845ef3c93be370b9d6fa4fd778
George Gordin <gcgordin@gmail.com>
2026-06-15 21:28:32 -0600

    basic server working remotely

---
+++ app.js
@@ -0,0 +1,10 @@
+//start listening for front end via express
+const express = require ('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+app.get('/', (req, res) => {
+  res.send('Received!')
+  console.log('received message')
+});
+app.listen(5555, () => console.log(`Listening on port 5555`));
