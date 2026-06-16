07ce3bc386722d9b6bc485cd9e8b474bedf2a40e
George Gordin <gcgordin@gmail.com>
2026-06-16 09:36:29 -0600

    checking live implementation

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 app.get('/', (req, res) => {
-  res.send('Received!')
+  res.send('Received on the live server!')
   console.log('received message')
 });
 app.listen(5555, () => console.log(`Listening on port 5555`));
